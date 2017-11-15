e5e76ffe464baaaace5e6ada2f9f3055cbbf0ba7
nitesh <nitesh.maravanti@gmail.com>
2017-11-15 18:20:36 +0530

    Removed server
    removed server

--- server.js
+++
@@ -1,7 +0,0 @@
-var billreaderapp = require('cordova'),
-app = cordova();
-app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-});

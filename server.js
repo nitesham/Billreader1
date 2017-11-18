4fb2031f6900dae1fa076e03aa772f22c1f0ec8f
nitesh <nitesh.maravanti@gmail.com>
2017-11-18 14:36:01 +0530

    change
    change

--- server.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-var billreaderapp = require('express'),
-billreaderapp = express();
-billreaderapp.use(express.static('www'));
-billreaderapp.set('port', process.env.PORT || 5000);
-billreaderapp.listen(billreaderapp.get('port'), function () {
-    console.log('Express server listening on port ' + billreaderapp.get('port'));
-});
